c314448c6ff81326865f467fcd5853f0546a4627
MagnusG03 <113421640+MagnusG03@users.noreply.github.com>
2025-09-20 02:04:57 +0200

    Add triceratops & platformer movement

---
+++ Assets/Scripts/PlatformerMovement.cs
@@ -0,0 +1,101 @@
+using System;
+using UnityEngine;
+
+public class DinoGameMovement : MonoBehaviour
+{
+    private float moveSpeed = 4f;
+    private float crouchSpeed = 2f;
+    private float walkSpeed = 4f;
+    private float runSpeed = 6f;
+    private float jumpForce = 6f;
+    public Transform groundCheckLeft;
+    public Transform groundCheckRight;
+    public Transform roofCheckLeft;
+    public Transform roofCheckRight;
+    private BoxCollider2D collider;
+    private Rigidbody2D rigidbody;
+    private Animator animator;
+    private float groundCheckRadius = 0.2f;
+    private float roofCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    SpriteRenderer spriteRenderer;
+    private bool isGrounded;
+    private bool isHeadColliding;
+    private bool facingRight = false;
+
+    float inputX;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
+        animator = GetComponentInChildren<Animator>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        inputX = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(rigidbody.linearVelocity.x));
+
+        if (facingRight && inputX < 0)
+        {
+            facingRight = false;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (!facingRight && inputX > 0)
+        {
+            facingRight = true;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+
+        animator.SetFloat("RunAnimationSpeed", Mathf.Abs(rigidbody.linearVelocity.x) / moveSpeed);
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.linearVelocity = new Vector2(rigidbody.linearVelocity.x, jumpForce);
+        }
+
+        isGrounded = Physics2D.OverlapCircle(groundCheckLeft.position, groundCheckRadius, groundLayer) || Physics2D.OverlapCircle(groundCheckRight.position, groundCheckRadius, groundLayer);
+        isHeadColliding = Physics2D.OverlapCircle(roofCheckLeft.position, roofCheckRadius, groundLayer) || Physics2D.OverlapCircle(roofCheckRight.position, roofCheckRadius, groundLayer);
+
+        if (isGrounded)
+        {
+            animator.SetBool("OnGround", true);
+        }
+        else
+        {
+            animator.SetBool("OnGround", false);
+        }
+
+        if (isGrounded && Input.GetKey(KeyCode.LeftControl))
+        {
+            animator.SetBool("Crouching", true);
+            moveSpeed = crouchSpeed;
+            collider.size = new Vector2(1.08f, 1.3f);
+            collider.offset = new Vector2(-0.06f, -0.14f);
+        }
+        else if (moveSpeed == crouchSpeed && !Input.GetKey(KeyCode.LeftControl) && !isHeadColliding)
+        {
+            animator.SetBool("Crouching", false);
+            moveSpeed = walkSpeed;
+            collider.size = new Vector2(1.08f, 1.923262f);
+            collider.offset = new Vector2(-0.06f, 0.1725656f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) && !animator.GetBool("Crouching") && isGrounded)
+        {
+            moveSpeed = runSpeed;
+        }
+        else if (!animator.GetBool("Crouching") && Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            moveSpeed = walkSpeed;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.linearVelocity = new Vector2(inputX * moveSpeed, rigidbody.linearVelocity.y);
+    }
+}
