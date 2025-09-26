7c3c25d5f1a40b029d3c6f2c7754fdae04610198
MagnusG03 <113421640+MagnusG03@users.noreply.github.com>
2025-09-26 19:02:03 +0200

    Fix hardcoded player height

--- Assets/Scripts/PlatformerMovement.cs
+++ Assets/Scripts/PlatformerMovement.cs
@@ -21,6 +21,8 @@ public class PlatformerMovement : MonoBehaviour
     private bool isGrounded;
     private bool isHeadColliding;
     private bool facingRight = false;
+    private Vector2 standingColliderSize;
+    private Vector2 standingColliderOffset;
 
     float inputX;
 
@@ -29,6 +31,8 @@ public class PlatformerMovement : MonoBehaviour
         rigidbody = GetComponent<Rigidbody2D>();
         collider = GetComponent<BoxCollider2D>();
         animator = GetComponentInChildren<Animator>();
+        standingColliderSize = collider.size;
+        standingColliderOffset = collider.offset;
     }
 
     void Update()
@@ -72,15 +76,15 @@ public class PlatformerMovement : MonoBehaviour
         {
             animator.SetBool("Crouching", true);
             moveSpeed = crouchSpeed;
-            collider.size = new Vector2(1.08f, 1.3f);
-            collider.offset = new Vector2(-0.06f, -0.14f);
+            collider.size = new Vector2(standingColliderSize.x, standingColliderSize.y * 2 / 3);
+            collider.offset = new Vector2(standingColliderOffset.x, standingColliderOffset.y - standingColliderSize.y / 6);
         }
         else if ((moveSpeed == crouchSpeed && !Input.GetKey(KeyCode.LeftControl) && !isHeadColliding) || (!isGrounded && !isHeadColliding && moveSpeed != runSpeed))
         {
             animator.SetBool("Crouching", false);
             moveSpeed = walkSpeed;
-            collider.size = new Vector2(1.08f, 1.8f);
-            collider.offset = new Vector2(-0.06f, 0.1725656f);
+            collider.size = standingColliderSize;
+            collider.offset = standingColliderOffset;
         }
 
         // Sprinting
